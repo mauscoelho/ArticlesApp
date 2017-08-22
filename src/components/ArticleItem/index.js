b06b8a006da16f06c37e4c4d86fd50bd45edb71b
Mauricio Coelho <mcimauricio.coelho@gmail.com>
2017-08-21 23:43:38 -0300

    :wrench: setup eslint

--- src/components/ArticleItem/index.js
+++ src/components/ArticleItem/index.js
@@ -1,14 +1,12 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React from 'react';
+import { View, Text } from 'react-native';
 
-const ArticleItem = () => {
-  return (
-    <View>
-      <Text>
-        {"simple test storybook"}
-      </Text>
-    </View>
-  );
-};
+const ArticleItem = () => (
+  <View>
+    <Text>
+      {'simple test storybook'}
+    </Text>
+  </View>
+);
 
 export default ArticleItem;

104c23118394b62bca78a748a5fead07a5d9dd78
Mauricio Coelho <mcimauricio.coelho@gmail.com>
2017-08-21 23:57:49 -0300

    :sparkles: show simple article item on storybook

--- src/components/ArticleItem/ArticleItem.stories.js
+++ src/components/ArticleItem/ArticleItem.stories.js
@@ -6,5 +6,14 @@ import { storiesOf } from '@storybook/react-native';
 
 import ArticleItem from './index';
 
-storiesOf('ArticleItem', module).add('default', () => <ArticleItem />);
-
+storiesOf('ArticleItem', module).add('default item', () =>
+  <ArticleItem
+    title={"Obama Offers Hopeful Vision While Noting Nation's Fears"}
+    author={'Graham Spencer'}
+    date={'05/26/2014'}
+    website={'MacStories'}
+    image={
+      'http://res.cloudinary.com/cheesecakelabs/image/upload/v1488993901/challenge/news_01_illh01.jpg'
+    }
+  />,
+);
--- src/components/ArticleItem/index.js
+++ src/components/ArticleItem/index.js
@@ -1,12 +1,39 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import propTypes from 'prop-types';
+import { View, Text, StyleSheet } from 'react-native';
 
-const ArticleItem = () => (
-  <View>
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F5FCFF',
+  },
+});
+
+const ArticleItem = ({ title, date, author, website, image }) =>
+  <View style={styles.container}>
     <Text>
-      {'simple test storybook'}
+      {title}
     </Text>
-  </View>
-);
+    <Text>
+      {date}
+    </Text>
+    <Text>
+      {author}
+    </Text>
+    <Text>
+      {website}
+    </Text>
+    <Text>
+      {image}
+    </Text>
+  </View>;
+
+ArticleItem.propTypes = {
+  title: propTypes.string.isRequired,
+  date: propTypes.string.isRequired,
+  author: propTypes.string.isRequired,
+  website: propTypes.string.isRequired,
+  image: propTypes.string.isRequired,
+};
 
 export default ArticleItem;

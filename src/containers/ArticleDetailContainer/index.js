6d7c5d8d96fe3c5f751a7380b979dc106b40593b
Mauricio Coelho <mcimauricio.coelho@gmail.com>
2017-09-05 00:32:31 -0300

    :sparkles: mark as read

--- src/containers/ArticleDetailContainer/index.js
+++ src/containers/ArticleDetailContainer/index.js
@@ -1,22 +1,36 @@
 import { connect } from 'react-redux';
-import { compose, mapProps } from 'recompose';
+import { compose, mapProps, withHandlers } from 'recompose';
 import ArticleDetail from '../../components/ArticleDetail';
-import { getArticlesAction } from '../../actions';
+import { getArticlesAction, markAsReadAction } from '../../actions';
 
 const mapStateToProps = (state, { navigation }) => ({
-  article: state.get('articles').find(article => article.get('title') === navigation.state.params.title),
+  article: state
+    .get('articles')
+    .find(article => article.get('title') === navigation.state.params.title),
 });
 
 const mapDispatchToProps = dispatch => ({
   getArticles: (args) => {
     dispatch(getArticlesAction(args));
   },
+  markAsRead: (args) => {
+    dispatch(markAsReadAction(args));
+  },
 });
+
+const markAsReadHandler = ({ markAsRead, title }) => () => {
+  markAsRead(title);
+};
 
 const enhance = compose(
   connect(mapStateToProps, mapDispatchToProps),
-  mapProps(({ article }) => ({
+  mapProps(({ article, ...rest }) => ({
     ...article.toJSON(),
-  })));
+    ...rest,
+  })),
+  withHandlers({
+    markAsReadHandler,
+  }),
+);
 
 export default enhance(ArticleDetail);

63c9ba27b01a27993a1500afc452f06d16815710
Mauricio Coelho <mcimauricio.coelho@gmail.com>
2017-09-04 23:36:21 -0300

    :rotating_light: update snapshots

--- src/components/FilterList/FilterList.stories.js
+++ src/components/FilterList/FilterList.stories.js
@@ -3,7 +3,17 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react-native';
 import { View, StyleSheet } from 'react-native';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+import { Map } from 'immutable';
 import FilterList from './index';
+
+const mockStore = configureStore();
+const store = mockStore(
+  Map({
+    settings: Map({ visibilityFilter: 'Default' }),
+  }),
+);
 
 const styles = StyleSheet.create({
   screen: {
@@ -20,8 +30,10 @@ const filters = [
   { title: 'Website' },
 ];
 
-storiesOf('FilterList', module).add('with default list', () =>
-  <View style={styles.screen}>
-    <FilterList filters={filters} />
-  </View>,
-);
+storiesOf('FilterList', module)
+  .addDecorator(getStory => <Provider store={store}>{getStory()}</Provider>)
+  .add('with default list', () => (
+    <View style={styles.screen}>
+      <FilterList filters={filters} />
+    </View>
+  ));

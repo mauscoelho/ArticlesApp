d37d178e4419e5f8f837d7435dd35bfa5b9c9789
Mauricio Coelho <mcimauricio.coelho@gmail.com>
2017-09-04 22:53:30 -0300

    :sparkles: make stateless

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { StackNavigator } from 'react-navigation';
+import { Button } from 'react-native';
 import store from './store';
 import Home from './screens/Home';
 import Detail from './screens/Detail';
@@ -9,6 +10,10 @@ import Filter from './screens/Filter';
 const Screens = StackNavigator({
   Home: {
     screen: Home,
+    navigationOptions: ({ navigation }) => ({
+      title: 'Home',
+      headerRight: <Button title={'Filter'} onPress={() => navigation.navigate('Filter')} />,
+    }),
   },
   Detail: {
     screen: Detail,
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,18 +1,8 @@
-import React, { Component } from 'react';
-import { Button } from 'react-native';
+import React from 'react';
 import propTypes from 'prop-types';
 import ArticlesListContainer from '../containers/ArticleListContainer';
 
-class Home extends Component {
-  static navigationOptions = ({ navigation }) => ({
-    title: 'Home',
-    headerRight: <Button title={'Filter'} onPress={() => navigation.navigate('Filter')} />,
-  })
-
-  render() {
-    return <ArticlesListContainer navigation={this.navigation} />;
-  }
-}
+const Home = ({ navigation }) => <ArticlesListContainer navigation={navigation} />;
 
 Home.propTypes = {
   navigation: propTypes.object.isRequired,
